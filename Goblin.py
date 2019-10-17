0ee0428744a38c4c88f23170152c2a5791b18fac
KonnichaWassup <47648470+KonnichaWassup@users.noreply.github.com>
2019-10-17 15:39:07 +0800

    Update Goblin.py
    Added startGame(), encounterBuild() with random monsters

--- Goblin.py
+++ Goblin.py
@@ -1,30 +1,57 @@
 import random
 
+numberMonster = 0
+
 class Monster:
-    def __init__(mon, Name, Type, HP, Atk, Acc):
+    def __init__(mon, Name, Type, HP, Atk, Acc, Ini):
         mon.Name = Name
         mon.Type = Type
         mon.HP = HP
         mon.Atk = Atk
         mon.Acc = Acc
-
-    def monGen(mon):
-        print("Added a " + mon.Name)
-        print("A " + mon.Type + "-type monster!")
-        print("Health: " + str(mon.HP))
-        print("Damage: " + str(mon.Atk))
-        print("Accuracy: " + str(mon.Acc))
-
-encounterMonsters = []
-
-m1 = Monster("Goblin", "goblin", 2, 3, 4)
-
-m1.monGen()
-encounterMonsters.append(m1.Name)
+        mon.Ini = Ini
 
 
 
-print(encounterMonsters)
+def monAdd(mon):
+    print("Added a " + mon.Name)
+    print("A " + mon.Type + "-type monster!")
+    print("Health: " + str(mon.HP))
+    print("Damage: " + str(mon.Atk))
+    print("Accuracy: " + str(mon.Acc))
+    print("Initiative: " + str(mon.Ini))
+
+def startGame():
+    global numberMonster
+    while True:
+        try:
+            numberMonster = int(input("How many monsters?"))
+        except(ValueError):
+            print("Enter a number!")
+        else:
+            print(numberMonster)
+            break
+
+gob = Monster("Goblin", "goblin", 2, 3, 1, 2)
+zom = Monster("Zombie", "undead", 3, 4, 3, 1)
+kob = Monster("Kobold", "reptile", 1, 3, 2, 3)
+monsterList = [gob, kob, zom]
+
+def buildEncounter():
+    global monsterList
+    global numberMonster
+    while numberMonster > 0:
+        monAdd(random.choice(monsterList))
+        numberMonster = numberMonster - 1
+    else:
+        print("Monsters added!")
+
+
+startGame()
+buildEncounter()
+#print(encMonsters)
+
+
 
 
 
